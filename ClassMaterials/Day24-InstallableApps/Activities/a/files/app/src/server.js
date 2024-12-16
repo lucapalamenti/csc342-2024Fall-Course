a22b8647ef61e63dead3fe85db85074b70ef2f43
lucapalamenti <luca.palamenti@gmail.com>
2024-12-16 18:00:24 -0500

    ok

--- ClassMaterials/Day24-InstallableApps/Activities/a/files/app/src/server.js
+++ ClassMaterials/Day24-InstallableApps/Activities/a/files/app/src/server.js
@@ -5,5 +5,6 @@ const routes = require('./routes');
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
+
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
