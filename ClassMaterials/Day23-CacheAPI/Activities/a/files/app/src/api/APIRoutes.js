0b66e1a63d67fec2de61fa789dbe5054853a90b9
lucapalamenti <luca.palamenti@gmail.com>
2024-12-18 09:08:21 -0500

    ok

--- ClassMaterials/Day23-CacheAPI/Activities/a/files/app/src/api/APIRoutes.js
+++ ClassMaterials/Day23-CacheAPI/Activities/a/files/app/src/api/APIRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 
 const apiRouter = express.Router();
 
-apiRouter.use(cookieParser());
 apiRouter.use(express.json());
 
 const { TokenMiddleware, generateToken, removeToken } = require('../middleware/TokenMiddleware');
